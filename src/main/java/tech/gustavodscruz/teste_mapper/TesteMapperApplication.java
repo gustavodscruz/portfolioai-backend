14e9a3e8558f5b1f8ce15abbb7e86cdbbe1cf50e
gustavodscruz <gustavodiasdsc@gmail.com>
2025-01-27 13:48:55 -0300

    first commit

---
+++ src/main/java/tech/gustavodscruz/teste_mapper/TesteMapperApplication.java
@@ -0,0 +1,13 @@
+package tech.gustavodscruz.teste_mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/gustavodscruz/teste_mapper/TesteMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.gustavodscruz.teste_mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
